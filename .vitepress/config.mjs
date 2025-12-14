89a03a97bd573fb567a8725635a4683ad8ea51c1
XJY4517 <1226458506@qq.com>
2025-12-14 19:53:05 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/my-record-site/', // 正确：和仓库名一致
-  title: '我的日常记录', // 修正：用英文引号
-  description: '记录每日饮食，训练等事项', // 修正：用英文引号
+  base: '/my-record-site/', 
+  title: '我的日常记录', 
+  description: '记录每日饮食，训练等事项', 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -21,7 +21,7 @@ export default defineConfig({
       }
     ],
     socialLinks: [
-      // 修正：指向你自己的仓库
+      
       { icon: 'github', link: 'https://github.com/XJY4517/my-record-site' }
     ]
   }
